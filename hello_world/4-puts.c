c8ac5f9c63bd4cfe42ac38530bcfc1d6ace3592c
Erkenoss <xavier.bertin6@gmail.com>
2023-06-15 08:57:56 -0700

    Add 4-

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Description pour main
+* Return: Always 0 (succes)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
