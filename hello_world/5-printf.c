7a3e671e92c4c0f589574f3af0df3abb0d183710
Erkenoss <xavier.bertin6@gmail.com>
2023-06-15 09:19:32 -0700

    Add 5-

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
