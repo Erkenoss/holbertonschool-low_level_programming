a971e532c5f272a210094aa6a5eeee35b7585983
Erkenoss <xavier.bertin6@gmail.com>
2023-06-19 00:58:34 -0700

    Fixed betty shit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,31 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
 int main(void)
 {
-		int n;
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		/* your code goes there */
-		if(n < 0){
-			printf("%d is negative", n);
-		}
-		else if(n == 0){
-			printf("%d is zero", n);
-		}
-		else {
-			printf("%d is popsitive", n)
-		}
-		return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf(" %d is negative", n);
+
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+
+	}
+	else
+	{
+		printf("%d is popsitive", n)
+	}
+	return (0);
 }
