5586413c2738e144207a917a498c1547bc2c4fc0
Erkenoss <xavier.bertin6@gmail.com>
2023-06-19 04:00:51 -0700

    Add 2-

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar (c);
+	return (0);
+}
