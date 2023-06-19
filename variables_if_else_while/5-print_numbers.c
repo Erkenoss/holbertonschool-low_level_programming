76f9cd95a6829bc0f167600b1d01017f4ce40636
Erkenoss <xavier.bertin6@gmail.com>
2023-06-19 07:14:44 -0700

    Add 5-

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+
+	printf("\n");
+
+	return (0);
+}
