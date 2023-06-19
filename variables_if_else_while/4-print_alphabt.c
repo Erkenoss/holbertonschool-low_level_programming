c31f1082775596f1348797c27bec05ef07bb9cef
Erkenoss <xavier.bertin6@gmail.com>
2023-06-19 05:56:45 -0700

    Add 4-

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 65; c <= 122; c++)
+	{
+		if (c == 101)
+		{
+			c--;
+		}
+		else if (c == 113)
+		{
+			c--;
+		}
+	}
+	return (0);
+}
