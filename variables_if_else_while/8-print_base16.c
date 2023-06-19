a172c52c3238fe11bd3b032fa60897d51758cb2f
Erkenoss <xavier.bertin6@gmail.com>
2023-06-19 09:10:15 -0700

    Add 8-

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 103; i++)
+	{
+		if (i == 58)
+			i = 97;
+
+		putchar(i);
+	}
+	putchar ('\n');
+	return (0);
+}
