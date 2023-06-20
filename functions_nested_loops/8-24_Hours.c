0df9b185966809432bb0cb84ac1d2078ca7ca1d7
Erkenoss <xavier.bertin6@gmail.com>
2023-06-20 15:37:53 -0700

    Add 8-var

---
+++ functions_nested_loops/8-24_Hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * * jack_bauer- show to 00.00 to 23.59 hours
+ * *
+ * * Return: Always 0.
+ * */
+
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	for (i = 0; i <= 10; i++)
+	{	
+	if (i == 10)
+		i = 0;
+		j++;
+	if (j == 6)
+		i = 0;
+		j = 0;
+		k++;
+	if (k == 10)
+		i = 0;
+		j = 0;
+		k = 0;
+		l++;
+	if (l == 2 && k == 3 && j == 5 && i == 9)
+		break;
+	}
+	_putchar(l + '0');
+	_putchar(k + '0');
+	_putchar(':');
+	_putchar(j + '0');
+	_putchar(i + '0');
+	_putchar('\n');
+}
