71409fcc9a2af213e9739ed9113517b059255520
Erkenoss <xavier.bertin6@gmail.com>
2023-06-20 08:03:06 -0700

    Add 6-

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - check the code
+* @i: imput int
+*
+* Return: Always 0.
+*/
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * (-1);
+		return (i);
+	}
+	else
+	{
+		return (i);
+	}
+}
