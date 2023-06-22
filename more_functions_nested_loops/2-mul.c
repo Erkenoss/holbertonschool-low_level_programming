fb5dd294f1a79e7601546b75cf581c73e5df87c0
Erkenoss <xavier.bertin6@gmail.com>
2023-06-22 08:29:27 -0700

    add 2-

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - Description pour main
+* @a: Whose idea was it to create Betty? Next on the third exercise.
+* @b: You have a good story to tell. So, why Betty is useless?
+* Maybe because there is nothing to learn with her?
+*
+* Return: Always 0 (succes)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
