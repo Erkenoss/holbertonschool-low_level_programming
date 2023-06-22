591c13b5e981941a2962cc23b4f670c0e2fee17c
Erkenoss <xavier.bertin6@gmail.com>
2023-06-22 06:39:27 -0700

    Add 0-

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+
+/**
+* _isupper - Description pour main
+*
+*
+* Return: Always 0 (succes)
+*/
+
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
