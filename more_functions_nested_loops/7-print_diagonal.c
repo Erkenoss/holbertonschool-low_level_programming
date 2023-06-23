42cd0ed6211f9e470eb5ee4dfed066721621300b
Erkenoss <xavier.bertin6@gmail.com>
2023-06-23 00:39:19 -0700

    Add 7-

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* print_diagonal - Description pour main
+* @n: Whose idea was it to create Betty? Next on the third exercise.
+* You will have a good story to tell. So, why Betty is useless?
+* Maybe because there is nothing to learn with her?
+* Oh! Yes, you are right, for take the good habit!
+* But now, we lost our time because we are affraid of you... Betty
+* What am i suppose to think? Good habit does not depend on you.
+* We all know that, you know... So, what am i suppose to think?
+* I think a simple thing. It's just totaly bullshit and lost of time.
+* Don't ask why... And think by yourself... isn't it?
+*
+* Have you think hard? Perfect. Because seriously, Betty is lost of time.
+* I explain seriously.
+* I understand why we need space between return and his value.
+* However, i cannot understand why i can't between printf and ().
+* It's just an example.
+*
+* It's just like... You see a white wall and you know his white.
+* Nobody can't tell you: This wall is black! But Betty... Yes. :'(
+* So you are right, you know you are right... But you are wrong.
+*
+* WHY?
+*
+* Return: Always 0 (succes)
+*/
+
+void print_diagonal(int n)
+{
+	int slash;
+	int espace;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (slash = 0; slash < n; slash++)
+	{
+		for (espace = 0; espace < slash; espace++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
