5ce355256e854e642a7893ace3d1c07e67840d3d
Erkenoss <xavier.bertin6@gmail.com>
2023-06-26 04:59:44 -0700

    Add 4-

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strlen - check the code
+* @s: a char pointer
+*
+* Return: Always 0.
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
