99cefbf5995cd5b5b38e956902ba53c0aa4075b2
Erkenoss <xavier.bertin6@gmail.com>
2023-06-26 03:02:26 -0700

    Add 3-

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* swap_int - check the code
+* _puts - check the code
 * @str: an char pointer
 *
 * Return: Always 0.
@@ -10,7 +10,7 @@
 void _puts(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
