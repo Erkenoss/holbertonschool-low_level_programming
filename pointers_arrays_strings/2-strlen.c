a6960ed8d6e6811d4085b0eb1642c2fe76d75c11
Erkenoss <xavier.bertin6@gmail.com>
2023-06-26 02:15:36 -0700

    Add 2-

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strlen - check the code
+* @s: a char pointer
+*
+* Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	
+	
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+
+}
