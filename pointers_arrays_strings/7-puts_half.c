21dd855502e4ce88a4355292bb328c97f1013f86
Erkenoss <xavier.bertin6@gmail.com>
2023-06-26 10:39:14 -0700

    add 7-

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,17 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 	{
-		len++; 
+		len++;
+	}
+
+	if (len % 2 != 0)
+	{
+		for (half = ((len - 1) / 2); half != len; half++)
+			_putchar(str[half]);
 	}
 
 	for (half = len / 2; half != len; half++)
-	{
 		_putchar(str[half]);
-	}
+
 	_putchar('\n');
 }
