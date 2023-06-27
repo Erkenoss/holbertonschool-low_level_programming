0650968087058e4c9b5e08e75d8fad72578ad3ad
Erkenoss <xavier.bertin6@gmail.com>
2023-06-27 05:27:24 -0700

    Add 1-2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strncat- check the code
+* @dest: a char pointer
+* @src: a char pointer
+* @n: an int bytes 
+*
+* Return: Always 0.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int len2 = 0;
+
+	while (dest[len] != '\0')
+		len++;
+
+	while (src[len2] != '\0')
+	{
+		if (len2 < n)
+			dest[len + len2] = src[len2];
+		len2++;
+	}
+	return (dest);
+}
