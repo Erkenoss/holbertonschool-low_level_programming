c52aa0ec6ecddd4f3804c9254f2d91c4ec8e6aaa
Erkenoss <xavier.bertin6@gmail.com>
2023-06-27 10:04:44 -0700

    Add 3-2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[len] && s2[len] != '\0')
 	{
 		if (s1[len] < s2[len])
-			result = - 1;
+			result = -1;
 
 		else if (s1[len] == s2[len])
 			result = 0;
