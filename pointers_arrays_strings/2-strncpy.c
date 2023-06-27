85e20d64d8ff68d3300919bc1f0284528aa532ae
Erkenoss <xavier.bertin6@gmail.com>
2023-06-27 06:56:51 -0700

    Add 2-2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strncpy- check the code
+* @dest: a char pointer
+* @src: a char pointer
+* @n: an int bytes 
+*
+* Return: Always 0.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+
+	while (len < n && src[len] != '\0')
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	while (len < n)
+	{
+		dest[len] = '\0';
+		len++;
+	}
+	return (dest);
+}
