3dda3f4c91590925e3e515b9a058bb4befc568b7
Erkenoss <xavier.bertin6@gmail.com>
2023-06-28 01:18:16 -0700

    Add 5-2

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* strcmp- check the code
+*
+* Return: Always 0.
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+			i++;
+		}
+	return (str);
+}
