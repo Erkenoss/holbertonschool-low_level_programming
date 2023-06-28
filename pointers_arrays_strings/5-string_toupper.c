e0ec260299590ee73201926e4568315e18d96c85
Erkenoss <xavier.bertin6@gmail.com>
2023-06-28 01:25:43 -0700

    add 5-2

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-* strcmp- check the code
+* string_toupper - check the code
+* @str: a char pointer
 *
-* Return: Always 0.
+* Return: always 0.
 */
 
 char *string_toupper(char *str)
@@ -11,10 +12,10 @@ char *string_toupper(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-			i++;
-		}
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
 	return (str);
 }
