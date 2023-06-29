21efff5ce7a182e77d11713960956a3c8415f4d3
Erkenoss <xavier.bertin6@gmail.com>
2023-06-29 14:16:46 -0700

    add 2-3

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strchr - check the code
+* @s: a char pointer
+* @c: a char argum
+*
+* Return: s, the pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (char *) s;
+
+		s++;
+	}
+	return NULL;
+}
