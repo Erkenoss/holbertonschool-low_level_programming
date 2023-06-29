07f20f760725b5ff10560b705680e0e4cb470cc2
Erkenoss <xavier.bertin6@gmail.com>
2023-06-29 13:40:07 -0700

    Add 1-3

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memcpy - check the code
+* @dest: a char pointer destination
+* @src: achr to cpy in dest
+* @n: nbr bytes for src to dest
+*
+* Return: dest, (the pointer)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++
+	}
+	return (dest);
+}
