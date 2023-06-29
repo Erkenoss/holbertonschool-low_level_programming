e27a34ed0a968a6eb1b09c3799c5ec864f063439
Erkenoss <xavier.bertin6@gmail.com>
2023-06-29 15:11:32 -0700

    Add 3-3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strspn - check the code
+* @s: a char pointer
+* @accept: pointer for take count on bytes in
+*
+* Return: s, the pointer
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int byt = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == *accept)
+			
+			byt++;
+		s++;
+	}
+	return (byt);
+}
