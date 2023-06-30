8ca53873ff0c719327c9bdcf82ec080cc282a864
Erkenoss <xavier.bertin6@gmail.com>
2023-06-30 07:57:17 -0700

    Add 7-3

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagsums - check the code
+ * @a: a int pointer
+ * @size: a int argum
+ *
+ * Return: the pointer
+ */
+void print_diagsums(int *a, int size)
+{
+	int r = 0;
+	int c = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+	
+	while (r < size)
+	{
+		sum1 += *(a + (r * size) + c);
+		sum2 += *(a + (r * size) + (size - 1 - c));
+		c++;
+		r++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
