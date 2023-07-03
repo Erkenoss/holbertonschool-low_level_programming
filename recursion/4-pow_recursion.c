1ab63f0a597f383038c87fa5a47698c348895cf6
Erkenoss <xavier.bertin6@gmail.com>
2023-07-03 07:57:20 -0700

    Add 4-

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 /**
 * _pow_recursion - check the code
 * @x: an int
-* @y: an int 
+* @y: an int
 * Return: Always 0.
 */
 
@@ -13,5 +13,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y < 0)
 		return (-1);
-	return (_pow_recursion (x, y - 1) * x);
+	return (_pow_recursion(x, y - 1) * x);
 }
