fc03c0bb9655a5d971a20061b1235ef78cd46825
Erkenoss <xavier.bertin6@gmail.com>
2023-07-05 07:21:53 -0700

    Add 4-

---
+++ argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* main - check the code
+* @argc: nbr of argu in main
+* @argv: list of argument since 0, the name of the prog
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int index = 0;
+	char *s;
+	int result = 0;
+	int index2;
+	while (index < argc)
+	{
+		s = argv[index];
+		index2 = 0;
+
+		while (s[index2] != '\0')
+		{
+			if (s[index2] <= '0' || s[index2] >= '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			index2++;
+		}
+		index++;
+	}
+	
+	for (index = 1; index < argc; index++)
+	{
+		result = result + atoi(argv[index]);
+	}
+	printf("%d", result);
+	return (0);
+}
