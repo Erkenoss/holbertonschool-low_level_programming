4e71ea0ffc8cd38e7d1dffbe5b880d43bba48761
Erkenoss <xavier.bertin6@gmail.com>
2023-07-05 05:59:17 -0700

    Add 3-

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* main - check the code
+* @argc: nbr of argu in main
+* @argv: list of argument since 0, the name of the prog
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int a = atoi(argv[2]);
+	int b = atoi(argv[3]);
+	int c = a * b;
+
+	if (argc == 3)
+	{
+		printf("%i\n", c);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
