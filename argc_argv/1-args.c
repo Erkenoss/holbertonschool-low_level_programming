2f71cff25f17141706381994c04ce87460074d84
Erkenoss <xavier.bertin6@gmail.com>
2023-07-05 01:26:19 -0700

    Add 1-

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+* main - check the code
+* @argc: nbr of argu in main
+* @argv: unused
+*
+* Return:
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
