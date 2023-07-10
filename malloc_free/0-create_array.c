115bf18e99031d8c74fcf8b2f4170e4b74bca8c1
Erkenoss <xavier.bertin6@gmail.com>
2023-07-10 02:21:12 -0700

    add 0-

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* create_array - create a temp array with malloc
+* @size: the size of the array
+* @c: the character passed to create_array
+*
+* Return: A pointer
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	
+	char *t;
+	unsigned int i;
+
+
+	if (size == 0) 
+		return (NULL);	
+
+	t = malloc(sizeof(char) * (size + 1));
+
+	if (t == NULL)
+		return (NULL);
+	
+	i = 0;
+
+	while (i < size)
+	{
+		t[i] = c;
+		i++;
+	}
+
+	return (t);
+}
