22dd05743922222a4795f3c341a7a12801ec60bc
Erkenoss <xavier.bertin6@gmail.com>
2023-07-12 05:39:33 -0700

    add 3-

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* array_range - return t after init all memb of size to 0
+*
+* @nmemb: nbr element in tab
+* @size: size of each element
+*
+* Return: t
+*/
+
+int *array_range(int min, int max)
+{
+	int *t = NULL;
+
+	if (min > max)
+		return (NULL);
+
+	t = malloc(sizeof(int) * (max - min) + 1);
+
+	if (t == NULL)
+		return (NULL);
+
+	while (min <= max)
+	{
+		t[min] = min;	
+		min++;
+	}
+	return (t);
+}
