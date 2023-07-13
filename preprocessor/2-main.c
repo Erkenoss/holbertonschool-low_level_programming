fc00600cd3ff6e7d5cd224a3a3a6c1696b1788b3
Erkenoss <xavier.bertin6@gmail.com>
2023-07-13 05:01:49 -0700

    Add 2-

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,11 +1,18 @@
 #ifndef MAIN_C
 #define MAIN_C
 #include <stdio.h>
-#endif 
+#endif
 
-int main ()
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+
+
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
