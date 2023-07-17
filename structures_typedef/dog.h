6f5bec3aa265b9eb6f80ddf3f1647f51240b8bf0
Erkenoss <xavier.bertin6@gmail.com>
2023-07-17 01:26:19 -0700

    fixed betty dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,6 +2,13 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct dog - structure with 3 variable
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
 
 struct dog
 {
