e515a358fb986e5ff23a325b935eed694cf21489
Erkenoss <xavier.bertin6@gmail.com>
2023-07-17 02:54:51 -0700

    Add 1-init

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,20 +2,20 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct dog - structure with 3 variable
  * @name: name of the dog
- * @owner: owner of the dog
  * @age: age of the dog
+ * @owner: owner of the dog
  */
-
-
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
