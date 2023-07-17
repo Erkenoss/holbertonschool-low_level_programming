0480211af7689ba356c835d3ea8f45bb54632601
Erkenoss <xavier.bertin6@gmail.com>
2023-07-17 02:57:40 -0700

    fixed 1-

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- * struct dog - structure with 3 variable
+ * init_dog - function use 3 variables and struct dog
  * @name: name of the dog
  * @owner: owner of the dog
  * @age: age of the dog
+ * @d: pointer point in struct dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
-	{	
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
