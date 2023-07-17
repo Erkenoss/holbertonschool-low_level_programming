b79d2f0711c77ef52e63bf9adf3f1826a9365816
Erkenoss <xavier.bertin6@gmail.com>
2023-07-17 05:33:28 -0700

    Add 2-

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 /**
- * init_dog - function use 3 variables and struct dog
- * @name: name of the dog
- * @owner: owner of the dog
- * @age: age of the dog
- * @d: pointer point in struct dog
+ *  print_dog - print the structure struct dog
+ *  @d: the pointer on struct dog
  */
 
 void print_dog(struct dog *d)
@@ -16,8 +13,8 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->age == 0)
-		d->age = d->age;	
+		d->age = d->age;
 	if (d->owner == NULL)
-		d->owner = "(nil)";		
+		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
