2d88ebfbd8c9cb1f6b53707710cf0f04678817da
Erkenoss <xavier.bertin6@gmail.com>
2023-07-18 04:12:46 -0700

    add 1-

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - initialize an array
+ * @array: element of the array
+ * @size: size of array
+ * @action: pointer on a void fonction give an int
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int index;
+	if (array == NULL || size == 0 || (*action) == NULL)
+		return;
+	for (index = 0; index < size; index++)
+		(*action)(array[index]);
+}
