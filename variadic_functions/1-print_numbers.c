7a472daa66eb834037b83c64e2da215ed4074b11
Erkenoss <xavier.bertin6@gmail.com>
2023-07-20 11:12:42 -0700

    add 1-

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - printf all the tab element
+ * @n: a constant unsigned, number of ... (elements)
+ * @separator: const char pointer between numbers, like ','
+ *
+ * Return: no return, void function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int nbr_arg;
+	va_list integer;
+
+	if (n == 0)
+		return;
+
+	va_start(integer, n);
+
+	for (nbr_arg = 0; nbr_arg < n; nbr_arg++)
+	{
+		printf("%d", va_arg(integer, int));
+		if (nbr_arg != n - 1 && separator != NULL)
+			printf("%s", separator);
+		else
+			printf("\n");
+	}
+	va_end(integer);
+}
