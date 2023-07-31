6ef4e98481e240b3233a4a22eab95f77ab71501b
Erkenoss <xavier.bertin6@gmail.com>
2023-07-31 12:24:30 -0700

    Add 2-add_node.c and change .h

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+
+/**
+ * add_node - function which return the adress of the new element or if failed
+ * @head: pointer of pointer of type list.
+ * @str: string passed for create a new node.
+ *
+ * Return: Always 0.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	list_t *actual = NULL;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		actual = *head;
+		while (actual->next != NULL)
+		{
+			actual = actual->next;
+		}
+	actual->next = new_node;
+	}
+	return (new_node);
+}
