771ffb7bc961213c72c0e964938619dc56678f29
Erkenoss <xavier.bertin6@gmail.com>
2023-08-01 08:08:58 -0700

    Add function in 6-

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -9,3 +9,19 @@
  */
 
 int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *temp = NULL;
+
+	if (head == NULL)
+		return (0);
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
