734e9053c664c1010912fb754afdd2a1fa92f823
Erkenoss <xavier.bertin6@gmail.com>
2023-08-01 00:51:18 -0700

    add 3-add_node_end.c

--- singly_linked_lists/0-print_list_end.c
+++ singly_linked_lists/0-print_list_end.c
@@ -1,22 +1,24 @@
 #include "lists.h"
 
 /**
- * print_list - print all the element of node
+ * print_lis_end - print all the element of node
  *
  * @h: pointer of constant type list_t
  *
  * Return: The size of the struct.
  */
 
-size_t print_list(const list_t *h)
+size_t print_list_end(const list_t *h)
 {
 	size_t count_element = 0;
 	const list_t *ptr = h;
 
 	if (h == NULL)
-	{
 		return (count_element);
-	}
+
+	while (ptr != NULL && ptr->next != NULL)
+		ptr = ptr->next;
+
 	while (ptr != NULL)
 	{
 		if (ptr->str != NULL)
@@ -28,7 +30,7 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", ptr->len, "(nil)");
 		}
 		count_element++;
-		ptr = ptr->next;
+		ptr = ptr->prev;
 	}
 	return (count_element);
 }
