c324cfa5fde256e3e69acd3c36b6f74136fd72b5
Erkenoss <xavier.bertin6@gmail.com>
2023-08-01 06:28:31 -0700

    add 3- and proto in header file

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function which return the adress
+ * of the new element or if failed
+ *
+ * @head: pointer of pointer of type list.
+ * @str: string passed for create a new node.
+ *
+ * Return: Always 0.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t *actual = NULL;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		actual = *head;
+		while (actual->next != NULL)
+		{
+			actual = actual->next;
+		}
+		actual->next = new_node;
+		new_node->prev = actual;
+	}
+	return (new_node);
+}
