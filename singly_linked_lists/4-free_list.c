124b81f6c01d870d556c65257c2d31eb851d4f08
Erkenoss <xavier.bertin6@gmail.com>
2023-08-01 01:58:30 -0700

    add 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - free all the node
+ * 
+ * @head: start of the list
+ * 
+ * Return: no return, void function
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp = NULL;
+	list_t *new = NULL;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		new = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = new;
+	}
+}
