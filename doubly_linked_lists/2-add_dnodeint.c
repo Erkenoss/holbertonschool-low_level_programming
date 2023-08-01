5a3aea6878eaa353da7fb71b56eda31e25b143cb
Erkenoss <xavier.bertin6@gmail.com>
2023-08-01 05:39:45 -0700

    Add 2-add_dnodeint.c and add proto in header

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint- function which return the adress
+ * of the new element or if failed
+ * @head: pointer of pointer of type list.
+ * @n: int passed in the node.
+ *
+ * Return: Always 0.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+
+	return (*head);
+}
