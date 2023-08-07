38216b1af10b0e8ea330e01b8465e2a4cacccede
Erkenoss <xavier.bertin6@gmail.com>
2023-08-07 07:43:39 -0700

    fixed 1-

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,16 +12,16 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
-       	size_t index = 0;
+	size_t index = 0;
 	ssize_t bytes_w = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return(-1);
+		return (-1);
 	while (text_content[index] != '\0')
 		index++;
 
