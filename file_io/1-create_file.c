0610cc0548791b5b3743f59168cebe53ec85ced0
Erkenoss <xavier.bertin6@gmail.com>
2023-08-07 07:46:36 -0700

    fixed null case for text_content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	while (text_content[index] != '\0')
+	while (text_content != NULL && text_content[index] != '\0')
 		index++;
 
 	bytes_w = write(fd, text_content, index);
