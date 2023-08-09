d9953741b2e51cc1785f6c03c7f670fe8b5d21e9
Erkenoss <xavier.bertin6@gmail.com>
2023-08-09 13:13:51 -0700

    add 3-

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,19 +13,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-		
+	hash_node_t *new_node;
+	hash_node_t *current;
+
 	if (key == NULL || key[0] == '\0')
 		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
 		return (0);
 
-	index = key_index((const unsigned char*)key, ht->size);
-
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
