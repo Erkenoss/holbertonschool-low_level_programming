2d84847ff238abac6ee331e346dbddc8eb4c6846
Erkenoss <xavier.bertin6@gmail.com>
2023-08-09 13:22:35 -0700

    fixed ht null

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,14 +16,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	hash_node_t *current;
 
+	if (ht == NULL)
+		return (0);
 	if (key == NULL || key[0] == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	current = malloc(sizeof(hash_node_t));
-	if (current == NULL)
-		return (0);
 
 	current = ht->array[index];
 	while (current != NULL)
