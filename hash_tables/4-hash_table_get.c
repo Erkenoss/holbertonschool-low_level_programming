e774e23da932bd16f63129ce232485aac5ef1a4b
Erkenoss <xavier.bertin6@gmail.com>
2023-08-10 02:15:44 -0700

    Add 4-

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - search the value 
+ * associated to the key
+ *
+ * @ht: pointer on the hash table
+ * @key: key associated for research
+ *
+ * Return: the value associated to the key
+ *
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return (NULL);
+
+	if (key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		else
+		{
+			current = current->next;
+		}
+	}
+
+	return (NULL);
+}
